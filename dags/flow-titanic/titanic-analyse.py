3367b4a149bf2ae6a5d811881dce4d043e8bf1a3
Sergio C Medina <smedina1304@gmail.com>
2021-08-01 17:17:06 -0300

    Testes flow-titanic

--- dags/flow-titanic/titanic-analyse.py
+++ dags/flow-titanic/titanic-analyse.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 import prefect
 import os
-from prefect import task, Flow
+from prefect import task, Flow, case
 from prefect.schedules import IntervalSchedule
 from datetime import datetime, timedelta
 from io import BytesIO
@@ -12,39 +12,48 @@ dirPath = '/Users/sergiomedina/Downloads/'
 retry_delay = timedelta(minutes=15)
 
 @task
-def downloadCSV():
+def download_csv():
     # Logger
     logger = prefect.context.get("logger")
     logger.info(f"Download CSV: {csvUrl}")
 
-    # Download CSV
-    filebytes = BytesIO(
-        requests.get(csvUrl, stream=True).content
-    )
+    # request get CSV
+    req = requests.get(csvUrl, stream=True)
+    if req.status_code == 200:
 
-    # Verificando Diretorio Output
-    if not dirPath.endswith('/'):
-        dirOut = dirPath + '/'
+        # Verificando Diretorio Output
+        if not dirPath.endswith('/'):
+            dirOut = dirPath + '/'
     
-    # Criando a pasta
-    dirOut = dirPath + 'CSV/'
-    os.makedirs(dirPath, exist_ok=True)
+        # Criando a pasta
+        dirOut = dirPath + 'CSV/'
+        os.makedirs(dirPath, exist_ok=True)
     
-    # Granvando o arquivo no diretório
-    dirOut = dirPath + 'titanic-train.csv'
-    with open(dirOut, 'w') as f:
-        f.write(filebytes.getbuffer())
-        f.close()
-    
-    filebytes.close()
+        # Granvando o arquivo no diretório
+        dirOut = dirPath + 'titanic-train.csv'
+        with open(dirOut, 'w') as f:
+            f.write(req.content)
+            f.close()
 
-    return dirOut
+        logger.info(f"Arquivo CSV gravado em: {dirOut}")
+
+    else:
+        dirOut = None
+    
+    return (req.status_code==200), dirOut
 
 
 @task
-def get_data(fileCSV):
+def ler_csv(fileCSV):
+    # Logger
+    logger = prefect.context.get("logger")
+    logger.info(f"Lendo arquivo CSV: {fileCSV}")
+
     # Carrega os dados do CSV em um Dataframe
     df = pd.read_csv(fileCSV)
+
+    logger.info(f"Dataset com [{df.shape[0]}]linhas e [{df.shape[0]}]colunas.")
+
     return df
 
 
@@ -68,13 +77,22 @@ def exibe_dataset(df):
     logger.info(df)
 
 
+@task
+def termino():
+    # Logger 
+    logger = prefect.context.get("logger")
+    logger.info("Finalização do fluxo.")
+
+
 # Definição do Fluxo (flow) e a sequencia das tasks
 with Flow("flow-titanic") as flow:
-    fcvs = downloadCSV()
-    df = get_data(fcvs)
-    med = calcula_media_idade(df)
-    exibe_media_calculada(df)
-    exibe_dataset(df)
+    check_download, fcvs = download_csv()
+
+    with case(check_download, True):
+        df = ler_csv(fcvs)
+        med = calcula_media_idade(df)
+        exibe_media_calculada(df)
+        exibe_dataset(df)
 
 
 # Registro do flow em um projeto
